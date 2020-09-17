7ca32be541fe89a7eaf17c75be4161abe69dcb94
Jumas-Cola <kbbyfl91@gmail.com>
2020-09-17 22:28:39 +0300

    upd pyredis_view

--- tor_spider/pyredis_view/pyredis.py
+++ tor_spider/pyredis_view/pyredis.py
@@ -4,6 +4,9 @@ from flask import render_template
 from flask import send_file
 from flask import request
 import json
+import re
+import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -30,17 +33,36 @@ def main():
 def export():
     res = request.args.get('res')
     file_name = 'export.{}'.format(res)
-    list_values = [{k.decode(): v.decode() for k, v in r.hgetall(k).items()} for k in r.keys()]
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+    list_values = ({k.decode(): v.decode() for k, v in r.hgetall(k).items()} for k in r.keys())
     if res == 'json':
         with open(file_name, 'w') as f:
-            json.dump(list_values, f)
+            json.dump([*list_values], f)
         return send_file(file_name)
     elif res == 'csv':
-        sorted_keys = sorted(list_values[0].keys())
+        sorted_keys = sorted(next(list_values).keys())
         with open(file_name, 'w') as f:
-            f.write(','.join(sorted_keys))
+            f.write(','.join(sorted_keys) + '\n')
             for row in list_values:
-                f.write(','.join(row[k] for k in sorted_keys))
+                f.write(','.join((re.sub('\W', ' ', row[k]) if k in row else '') for k in sorted_keys) + '\n')
+        return send_file(file_name)
+    elif res == 'db':
+        conn = sqlite3.connect(file_name)
+        cur = conn.cursor()
+        cur.execute("""CREATE TABLE Export(
+           id INT PRIMARY KEY,
+           {});
+        """.format(','.join('{} TEXT'.format(i) for i in next(list_values).keys())))
+
+        for n, row in enumerate(list_values):
+            heads, vals = ['id'], ['"{}"'.format(n)]
+            for h, v in row.items():
+                heads.append(h)
+                vals.append('"{}"'.format(re.sub(r'"|\'', '', v)))
+
+            cur.execute('INSERT INTO Export({}) VALUES ({});'.format(','.join(heads), ','.join(vals)))
+        conn.commit()
         return send_file(file_name)
     return ''
 
